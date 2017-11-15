3c6f46e2167391c8aff741b27185fd04f334f5be
ConanKapoor <kapoor.shivam88@gmail.com>
2017-11-15 18:36:52 +0530

    Output folder logic made

--- Scrape.py
+++ Scrape.py
@@ -3,7 +3,17 @@
 # Importing libraries
 from bs4 import BeautifulSoup
 import urllib.request
-import re
+import os,re
+
+# Creating Output folder
+if (os.path.exists("Output")):
+    delete = str('rm -r Output')
+    os.system(delete)
+    os.makedirs("Output")
+else:
+    os.makedirs("Output")
+
+#################### Scraping Logic Below ###########################
 
 # Collecting html content.
 url = "https://gallery.123telugu.com/content/reviews/main/more_reviews_Telugu.html"
@@ -22,3 +32,10 @@ for tag in divTag:
     links.append(tag.get('href'))
 
 #################### Now links list contains all the links to scrape###########################
+
+for link in links:
+    # Opening and scraping the data from every page.
+    request = urllib.request.Request(link)
+    response = urllib.request.urlopen(request)
+    soup = BeautifulSoup(response.read(),'html.parser')
+    print(soup)
