d0c923ed7302307a421122e0cd2a45f576dab985
ConanKapoor <kapoor.shivam88@gmail.com>
2017-11-15 18:17:34 +0530

    All the links to scrape collected

--- Scrape.py
+++ Scrape.py
@@ -5,10 +5,20 @@ from bs4 import BeautifulSoup
 import urllib.request
 import re
 
-#Collecting html content.
+# Collecting html content.
 url = "https://gallery.123telugu.com/content/reviews/main/more_reviews_Telugu.html"
 request = urllib.request.Request(url)
 response = urllib.request.urlopen(request)
 
-#Using BeautifulSoup to parse html object response.
-page = BeautifulSoup(response.read(),'html.parser')
+# Using BeautifulSoup to parse html object response.
+soup = BeautifulSoup(response.read(),'html.parser')
+
+# Finding Specific DIV tag data
+links = []
+divTag = soup.find_all("a", {"class": "NormalText"})
+
+# Collecting all the links from given webpage
+for tag in divTag:
+    links.append(tag.get('href'))
+
+#################### Now links list contains all the links to scrape###########################
